4ef5cb2206f820f246763e6796cceac04c08ce42
James Driscoll <thejamesdriscoll@gmail.com>
2022-01-28 13:46:50 -0600

    added script.js to make search functional

---
+++ script.js
@@ -0,0 +1,23 @@
+const searchBtn = document.querySelector('[data-search-button]')
+const searchInput = document.querySelector('[data-search-input]')
+
+function search() {
+    window.open(`https://www.google.com/search?q=${searchInput.value}`,'_blank')
+    
+}
+function clearInput(target) {
+    return target.value = ''
+}
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    search()
+    clearInput(searchInput)
+})
+searchInput.addEventListener('keypress', e => {
+    if (e.keyCode === 13) {
+        e.preventDefault()
+        search()
+        clearInput(searchInput)
+    }
+
+})
